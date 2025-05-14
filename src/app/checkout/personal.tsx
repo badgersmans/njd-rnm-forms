b1d7b915d705030be2b5d7939e83c54e3290bc81
Shawn Law <shawnlaw6669@gmail.com>
2025-05-14 19:45:06 +0800

    initial forms, reusable text input

--- src/app/checkout/personal.tsx
+++ src/app/checkout/personal.tsx
@@ -1,8 +1,11 @@
 import { View, Text, StyleSheet } from 'react-native'
 import CustomButton from '../../components/CustomButton'
 import { router } from 'expo-router'
+import { useState } from 'react'
+import CustomTextInput from '../../components/CustomTextInput'
 
 export default function PersonalDetailsForm() {
+  const [fullname, setFullname] = useState('')
 
   const onNext = () => {
     // validate the form
@@ -13,6 +16,22 @@ export default function PersonalDetailsForm() {
 
   return (
     <View style={styles.container}>
+
+      <CustomTextInput label="Full name" placeholder='Tuk Kun'/>
+      <CustomTextInput 
+        label="Address" 
+        placeholder='Bangkok'
+        multiline={true}
+        style={{minHeight: 80}}
+      />
+
+      <View style={{flexDirection: 'row', gap: 20}}>
+        <CustomTextInput label="City" placeholder='City' containerStyle={{flex: 1}} />
+        <CustomTextInput label="Postcode" placeholder='1234' containerStyle={{flex: 1}}/>
+      </View>
+
+      <CustomTextInput label="Phone number" placeholder='12345678' inputMode='tel'/>
+
       <CustomButton title='Next' onPress={onNext} style={styles.button}/>
     </View>
   )
@@ -22,7 +41,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    padding: 10
+    padding: 10,
+    gap: 5
   },
   button: {
     marginTop: 'auto',
